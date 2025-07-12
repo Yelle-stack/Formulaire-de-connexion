e815f7528f8e6d82c48325dc92e6ecd0ef0ae8f5
Jelena <jelena33.bliss@gmail.com>
2025-07-12 08:46:13 +0200

    init

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Formulaire de connexion</title>
+</head>
+<body>
+    <section>
+        <h1>Connexion</h1>
+        <form action="#" method="POST">
+            <input type="text" name="email">
+            <input type="password" name="mdp">
+        </form>
+    </section>
+</body>
+</html>
